4c7caa1154e567299944c8f6aa3e1e1126ed3f33
Doug Kintop <dkintop@hotmail.com>
2019-11-26 14:07:21 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,7 +2,24 @@ class Ride < ActiveRecord::Base
     belongs_to :user
     belongs_to :attraction
 
+
+    
+
+
     def take_ride
-        
+      
+        if  self.user.meets_ride_requirements(self.attraction) == "true"
+            self.user.tickets -= self.attraction.tickets
+            binding.pry
+        else
+            self.user.meets_ride_requirements(self.attraction)
+        end
+         
     end
+
+    
+
+
+    
+    
 end
