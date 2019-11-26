91f508436132fb0624a0c9cd4d646e309e07beac
Doug Kintop <dkintop@hotmail.com>
2019-11-26 16:02:50 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,14 @@ class UsersController < ApplicationController
     end
 
     def create
-      @user = User.create(user_params)
-
+      if @user = User.create(user_params)
+        session[:user_id] = @user.id
       redirect_to user_path(@user)
+      end
+    end
+
+    def show
+      @user = User.find_by_id(params[:id])
     end
 
     private
