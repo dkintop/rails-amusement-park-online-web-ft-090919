ba5b5ce08b81e77802da0a66bbddccff8bee7b56
Doug Kintop <dkintop@hotmail.com>
2019-11-26 14:27:00 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -7,19 +7,41 @@ class Ride < ActiveRecord::Base
 
 
     def take_ride
-      
-        if  self.user.meets_ride_requirements(self.attraction) == "true"
-            self.user.tickets -= self.attraction.tickets
-            binding.pry
-        else
-            self.user.meets_ride_requirements(self.attraction)
+        if !self.check_tickets && !self.check_height
+            self.not_enough_tickets + self.not_tall_enough 
+        
+        elsif !self.check_tickets && self.check_height
+            self.not_enough_tickets
+        elsif !self.check_height && self.check_tickets
+            self.not_tall_enough
         end
-         
+
     end
 
     
+    def check_tickets
+        if self.user.tickets >= self.attraction.tickets
+            true
+        else
+            false
+        end
+    end
 
+    def check_height
+        if self.user.height >= self.attraction.min_height
+            true
+        else
+            false
+        end
+    end
 
+    def not_enough_tickets
+        "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+    end
+
+    def not_tall_enough
+        "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    end
     
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,34 +1,6 @@
 class User < ActiveRecord::Base
     has_many :rides
 
-    def has_enough_tickets(attraction)
-        if self.tickets >= attraction.tickets
-            return true
-        else
-            return false
-        end
-    end
-
-    def is_tall_enough(attraction)
-        if self.height >= attraction.min_height
-            true
-        else
-             false
-        end
-    end
-
-    def meets_ride_requirements(attraction)
-       if self.is_tall_enough(attraction) && self.has_enough_tickets(attraction)
-            "true"
-       elsif !self.has_enough_tickets(attraction) &&self.is_tall_enough(attraction)
-            "Sorry. You do not have enough tickets to ride the #{attraction.name}."
-        elsif !self.is_tall_enough(attraction) && self.has_enough_tickets(attraction)
-            "Sorry. You are not tall enough to ride the #{attraction.name}."
-        elsif !self.is_tall_enough(attraction) && !self.has_enough_tickets(attraction)
-            "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}." 
-
-       end
-    end
-
+  
 
 end
